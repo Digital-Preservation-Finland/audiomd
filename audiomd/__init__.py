7a9b1ee7e6bead2cc1799fbf8271a84f3dec1b44
Juha Lehtonen <juha.lehtonen@csc.fi>
2023-03-30 08:28:31 +0300

    KDKPAS-2987 Bump version and remove Python 2 support officially.

--- audiomd/__init__.py
+++ audiomd/__init__.py
@@ -1,2 +1,7 @@
-# flake8: noqa
-from .audiomd import *
+"""Import basic library files for making convenient use of the library
+possible.
+"""
+__version__ = '0.10'
+
+# flake8 doesn't like these imports, but they are needed for other repos
+from .audiomd import *  # noqa: F401,F403
